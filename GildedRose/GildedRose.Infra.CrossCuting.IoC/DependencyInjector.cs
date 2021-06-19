8a5736cd4e61fedadc73658cd40f06fa7367d250
Lucas Michael <lucasmichael2109@hotmail.com>
2021-06-19 02:02:16 -0300

    Arquivos iniciais para arquitetura DDD
    Depois de Refatorar o projeto inicial, agora o GildedRose será transformado em um projeto com arquitetura DDD, estes são os arquivos iniciais. (Não está compilável)

---
+++ GildedRose/GildedRose.Infra.CrossCuting.IoC/DependencyInjector.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace GildedRose.Infra.CrossCuting.IoC
+{
+    public class DependencyInjector
+    {
+        public IServiceProvider ServiceProvider { get; set; }
+
+        private static readonly Lazy<DependencyInjector>
+        lazy =
+        new Lazy<DependencyInjector>
+            (() => new DependencyInjector());
+
+        public static DependencyInjector Instance { get { return lazy.Value; } }
+
+        private DependencyInjector()
+        {
+            ServiceProvider = GetServiceProvider();
+        }
+
+        public T GetService<T>()
+        {
+            return ServiceProvider.GetService<T>();
+        }
+        private static IServiceProvider GetServiceProvider()
+        {
+            return new ServiceCollection()
+                .AddTransient<IGildedRose, GildedRose>()
+                .BuildServiceProvider();
+        }
+    }
+}
---
+++ GildedRose/GildedRose/Configurations/Setup/DependencyInjectionSetup.cs
@@ -0,0 +1,12 @@
+﻿using GildedRose.Infra.CrossCuting.IoC;
+
+namespace GildedRose.Configurations.Setup
+{
+    public static class DependencyInjectionSetup
+    {
+        public static IGildedRose AddDependencyInjectionSetup()
+        {
+            return DependencyInjector.Instance.GetService<IGildedRose>();
+        }
+    }
+}
