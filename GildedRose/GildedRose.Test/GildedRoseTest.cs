15c972d5979307ebdf7fd88dafa15a7359d50084
Lucas Michael <lucasmichael2109@hotmail.com>
2021-06-19 14:17:16 -0300

    Teste de  todas as combinações refatorado
    TEste que valida todas as combinações de qualidade e sellIn refatorado.

--- GildedRose/GildedRose.Test/GildedRoseTest.cs
+++ GildedRose/GildedRose.Test/GildedRoseTest.cs
@@ -2,6 +2,7 @@
 using ApprovalTests.Reporters;
 using GildedRose.Application.Services;
 using GildedRose.Domain.Models.Base;
+using GildedRose.Domain.Models.Items;
 using NUnit.Framework;
 using System.Collections.Generic;
 
@@ -14,25 +15,141 @@ namespace GildedRose.Test
         [Test]
         public void UpdateQuality()
         {
-            string[] nameTestValues = { "+5 Dexterity Vest", "Aged Brie",
-                "Backstage passes to a TAFKAL80ETC concert", "Elixir of the Mongoose", "Sulfuras, Hand of Ragnaros", "Conjured Mana Cake" };
-            int[] sellInTestValues = { -1, 1, 5, 7, 11 };
-            int[] qualityTestValues = { 0, 1, 49, 51 };
+            IList<CompleteItem> testList = new List<CompleteItem> {
+                new Default("+5 Dexterity Vest", -1, 0),
+                new Default("+5 Dexterity Vest", -1, 1),
+                new Default("+5 Dexterity Vest", -1, 49),
+                new Default("+5 Dexterity Vest", -1, 51),
+                new Default("+5 Dexterity Vest", 1, 0),
+                new Default("+5 Dexterity Vest", 1, 1),
+                new Default("+5 Dexterity Vest", 1, 49),
+                new Default("+5 Dexterity Vest", 1, 51),
+                new Default("+5 Dexterity Vest", 5, 0),
+                new Default("+5 Dexterity Vest", 5, 1),
+                new Default("+5 Dexterity Vest", 5, 49),
+                new Default("+5 Dexterity Vest", 5, 51),
+                new Default("+5 Dexterity Vest", 7, 0),
+                new Default("+5 Dexterity Vest", 7, 1),
+                new Default("+5 Dexterity Vest", 7, 49),
+                new Default("+5 Dexterity Vest", 7, 51),
+                new Default("+5 Dexterity Vest", 11, 0),
+                new Default("+5 Dexterity Vest", 11, 1),
+                new Default("+5 Dexterity Vest", 11, 49),
+                new Default("+5 Dexterity Vest", 11, 51),
+                new AgedBrie("Aged Brie",-1,0),
+                new AgedBrie("Aged Brie",-1,1),
+                new AgedBrie("Aged Brie",-1,49),
+                new AgedBrie("Aged Brie",-1,51),
+                new AgedBrie("Aged Brie",1,0),
+                new AgedBrie("Aged Brie",1,1),
+                new AgedBrie("Aged Brie",1,49),
+                new AgedBrie("Aged Brie",1,51),
+                new AgedBrie("Aged Brie",5,0),
+                new AgedBrie("Aged Brie",5,1),
+                new AgedBrie("Aged Brie",5,49),
+                new AgedBrie("Aged Brie",5,51),
+                new AgedBrie("Aged Brie",7,0),
+                new AgedBrie("Aged Brie",7,1),
+                new AgedBrie("Aged Brie",7,49),
+                new AgedBrie("Aged Brie",7,51),
+                new AgedBrie("Aged Brie",11,0),
+                new AgedBrie("Aged Brie",11,1),
+                new AgedBrie("Aged Brie",11,49),
+                new AgedBrie("Aged Brie",11,51),
+                new BackstagePasses("Backstage passes to a TAFKAL80ETC concert",-1,0),
+                new BackstagePasses("Backstage passes to a TAFKAL80ETC concert",-1,1),
+                new BackstagePasses("Backstage passes to a TAFKAL80ETC concert",-1,49),
+                new BackstagePasses("Backstage passes to a TAFKAL80ETC concert",-1,51),
+                new BackstagePasses("Backstage passes to a TAFKAL80ETC concert",1,0),
+                new BackstagePasses("Backstage passes to a TAFKAL80ETC concert",1,1),
+                new BackstagePasses("Backstage passes to a TAFKAL80ETC concert",1,49),
+                new BackstagePasses("Backstage passes to a TAFKAL80ETC concert",1,51),
+                new BackstagePasses("Backstage passes to a TAFKAL80ETC concert",5,0),
+                new BackstagePasses("Backstage passes to a TAFKAL80ETC concert",5,1),
+                new BackstagePasses("Backstage passes to a TAFKAL80ETC concert",5,49),
+                new BackstagePasses("Backstage passes to a TAFKAL80ETC concert",5,51),
+                new BackstagePasses("Backstage passes to a TAFKAL80ETC concert",7,0),
+                new BackstagePasses("Backstage passes to a TAFKAL80ETC concert",7,1),
+                new BackstagePasses("Backstage passes to a TAFKAL80ETC concert",7,49),
+                new BackstagePasses("Backstage passes to a TAFKAL80ETC concert",7,51),
+                new BackstagePasses("Backstage passes to a TAFKAL80ETC concert",11,0),
+                new BackstagePasses("Backstage passes to a TAFKAL80ETC concert",11,1),
+                new BackstagePasses("Backstage passes to a TAFKAL80ETC concert",11,49),
+                new BackstagePasses("Backstage passes to a TAFKAL80ETC concert",11,51),
+                new Default("Elixir of the Mongoose",-1,0),
+                new Default("Elixir of the Mongoose",-1,1),
+                new Default("Elixir of the Mongoose",-1,49),
+                new Default("Elixir of the Mongoose",-1,51),
+                new Default("Elixir of the Mongoose",1,0),
+                new Default("Elixir of the Mongoose",1,1),
+                new Default("Elixir of the Mongoose",1,49),
+                new Default("Elixir of the Mongoose",1,51),
+                new Default("Elixir of the Mongoose",5,0),
+                new Default("Elixir of the Mongoose",5,1),
+                new Default("Elixir of the Mongoose",5,49),
+                new Default("Elixir of the Mongoose",5,51),
+                new Default("Elixir of the Mongoose",7,0),
+                new Default("Elixir of the Mongoose",7,1),
+                new Default("Elixir of the Mongoose",7,49),
+                new Default("Elixir of the Mongoose",7,51),
+                new Default("Elixir of the Mongoose",11,0),
+                new Default("Elixir of the Mongoose",11,1),
+                new Default("Elixir of the Mongoose",11,49),
+                new Default("Elixir of the Mongoose",11,51),
+                new Sulfuras("Sulfuras, Hand of Ragnaros",-1,0),
+                new Sulfuras("Sulfuras, Hand of Ragnaros",-1,1),
+                new Sulfuras("Sulfuras, Hand of Ragnaros",-1,49),
+                new Sulfuras("Sulfuras, Hand of Ragnaros",-1,51),
+                new Sulfuras("Sulfuras, Hand of Ragnaros",1,0),
+                new Sulfuras("Sulfuras, Hand of Ragnaros",1,1),
+                new Sulfuras("Sulfuras, Hand of Ragnaros",1,49),
+                new Sulfuras("Sulfuras, Hand of Ragnaros",1,51),
+                new Sulfuras("Sulfuras, Hand of Ragnaros",5,0),
+                new Sulfuras("Sulfuras, Hand of Ragnaros",5,1),
+                new Sulfuras("Sulfuras, Hand of Ragnaros",5,49),
+                new Sulfuras("Sulfuras, Hand of Ragnaros",5,51),
+                new Sulfuras("Sulfuras, Hand of Ragnaros",7,0),
+                new Sulfuras("Sulfuras, Hand of Ragnaros",7,1),
+                new Sulfuras("Sulfuras, Hand of Ragnaros",7,49),
+                new Sulfuras("Sulfuras, Hand of Ragnaros",7,51),
+                new Sulfuras("Sulfuras, Hand of Ragnaros",11,0),
+                new Sulfuras("Sulfuras, Hand of Ragnaros",11,1),
+                new Sulfuras("Sulfuras, Hand of Ragnaros",11,49),
+                new Sulfuras("Sulfuras, Hand of Ragnaros",11,51),
+                new ManaCake("Conjured Mana Cake",-1,0),
+                new ManaCake("Conjured Mana Cake",-1,1),
+                new ManaCake("Conjured Mana Cake",-1,49),
+                new ManaCake("Conjured Mana Cake",-1,51),
+                new ManaCake("Conjured Mana Cake",1,0),
+                new ManaCake("Conjured Mana Cake",1,1),
+                new ManaCake("Conjured Mana Cake",1,49),
+                new ManaCake("Conjured Mana Cake",1,51),
+                new ManaCake("Conjured Mana Cake",5,0),
+                new ManaCake("Conjured Mana Cake",5,1),
+                new ManaCake("Conjured Mana Cake",5,49),
+                new ManaCake("Conjured Mana Cake",5,51),
+                new ManaCake("Conjured Mana Cake",7,0),
+                new ManaCake("Conjured Mana Cake",7,1),
+                new ManaCake("Conjured Mana Cake",7,49),
+                new ManaCake("Conjured Mana Cake",7,51),
+                new ManaCake("Conjured Mana Cake",11,0),
+                new ManaCake("Conjured Mana Cake",11,1),
+                new ManaCake("Conjured Mana Cake",11,49),
+                new ManaCake("Conjured Mana Cake",11,51)
+            };
 
-            CombinationApprovals.VerifyAllCombinations(UpdateQualityTestSender, nameTestValues, sellInTestValues, qualityTestValues);
+            CombinationApprovals.VerifyAllCombinations(UpdateQualityTestSender, testList);
         }
 
-        private string UpdateQualityTestSender(string name, int sellIn, int quality)
+        private string UpdateQualityTestSender(CompleteItem item)
         {
-            // TODO: Refatorar teste
-            //IList<Item> items = new List<Item> { new Item { Name = name, SellIn = sellIn, Quality = quality } };
-            //GildedRoseService app = new GildedRoseService();
-            //app.SetItems(items);
+            List<CompleteItem> items = new List<CompleteItem> { item };
+            GildedRoseService app = new GildedRoseService();
+            app.SetItems(items);
 
-            //app.UpdateQuality();
+            app.UpdateQuality();
 
-            //return app.GetItems()[0].ToString();
-            return string.Empty;
+            return app.GetItems()[0].ToString();
         }
     }
 }
